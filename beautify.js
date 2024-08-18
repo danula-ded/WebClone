e6231c2856395ecbf150d79cf777d668f778e6dc
Stupishin Daniil <stupishin1597@yandex.ru>
2024-08-18 17:06:37 +0300

    save

--- beautify.js
+++ beautify.js
@@ -1,19 +1,44 @@
-const beautify = require('js-beautify');
-const beautify_html = require('js-beautify').html;
 const beautify_css = require('js-beautify').css;
+const beautify_js = require('js-beautify').js;
+const beautify_html = require('js-beautify').html;
 
 const fs = require('fs');
 const path = require('path');
 
-const dirPath = __dirname + "/node/css";
-const files = fs.readdirSync(dirPath);
+function beautifyFiles(dirPath = __dirname) {
+    const files = fs.readdirSync(dirPath);
 
-files.forEach((file) => {
-    const filePath = path.join(dirPath, file);
-    const stats = fs.statSync(filePath);
-    if (stats.isFile()) {
-        const cssContent = fs.readFileSync(filePath, 'utf8');
-        const beautifiedCss = beautify_css(cssContent);
-        fs.writeFileSync(filePath, beautifiedCss);
-    }
-});
+    files.forEach((file) => {
+        const filePath = path.join(dirPath, file);
+        const stats = fs.statSync(filePath);
+
+        if (stats.isFile()) {
+            const ext = path.extname(file);
+
+            let beautifiedContent;
+            const content = fs.readFileSync(filePath, 'utf8');
+
+            switch (ext) {
+                case '.css':
+                    beautifiedContent = beautify_css(content);
+                    break;
+                case '.js':
+                    beautifiedContent = beautify_js(content);
+                    break;
+                case '.html':
+                    beautifiedContent = beautify_html(content);
+                    break;
+                default:
+                    return; // Пропустить файл, если расширение не соответствует
+            }
+
+            fs.writeFileSync(filePath, beautifiedContent);
+        } else if (stats.isDirectory()) {
+            // Рекурсивно форматировать файлы в поддиректориях
+            beautifyFiles(filePath);
+        }
+    });
+}
+
+// Пример использования:
+beautifyFiles(__dirname + "/node");
--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-const beautify_js = require('js-beautify');
-const beautify_html = require('js-beautify').html;
-const beautify_css = require('js-beautify').css;
-
-
 import('website-scraper').then(({ default: scrape }) => {
     let options = {
         urls: ['https://nodejs.org/'],
