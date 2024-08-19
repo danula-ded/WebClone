74a3fbf1f3239a21da6a395766d2e138b42bc6a1
Stupishin Daniil <stupishin1597@yandex.ru>
2024-08-20 00:06:52 +0300

    save

--- index.js
+++ index.js
@@ -1,36 +1,40 @@
-const beautifyFiles = require('./beautify.js');
-const archiveAndDelete = require('./arhivator.js');
+const beautifyFiles = require('./js/beautify.js');
+const archiveAndDelete = require('./js/arhivator.js');
 
-const nameDirectory = "node";
+export function cloneWeb() {
+    const nameDirectory = "node";
+    const url = document.getElementById("url-input").value
+    console.log("init")
+    import('website-scraper').then(({ default: scrape }) => {
+        let options = {
+            urls: [`./${url}`],
+            directory: `./${nameDirectory}`,
 
-import('website-scraper').then(({ default: scrape }) => {
-    let options = {
-        urls: ['https://nodejs.org/'],
-        directory: `./${nameDirectory}`,
-
-        // recursive: true,
-        // maxDepth: 1
-        // urlFilter: function(url){
-        //     if(url.indexOf(websiteUrl) === 0){
-        //         console.log(`URL ${ url } matches ${ websiteUrl } `);
-        //         return true;
-        //     }
-        //     return false;
-        // },
-    };
+            // recursive: true,
+            // maxDepth: 1
+            // urlFilter: function(url){
+            //     if(url.indexOf(websiteUrl) === 0){
+            //         console.log(`URL ${ url } matches ${ websiteUrl } `);
+            //         return true;
+            //     }
+            //     return false;
+            // },
+        };
 
 
-    // копируем файлы через scrapper
-    scrape(options).then(() => {
-        console.log("Веб-сайт успешно скачан");
-    }).then(() => {
-        // все файлы чаще 
-        beautifyFiles(__dirname + `/${nameDirectory}`);
-        console.log("Веб-сайт успешно отредактирован")
-    }).then(() => {
-        archiveAndDelete(__dirname + `/${nameDirectory}`);
-        console.log("Веб-сайт успешно архивирован");
-    }).catch((err) => {
-        console.log("Произошла ошибка", err);
+        // копируем файлы через scrapper
+        scrape(options).then(() => {
+            console.log("Веб-сайт успешно скачан");
+        }).then(() => {
+            // все файлы чаще 
+            beautifyFiles(__dirname + `/${nameDirectory}`);
+            console.log("Веб-сайт успешно отредактирован")
+        }).then(() => {
+            // архивирует файлы и удаляет папку из fs
+            archiveAndDelete(__dirname + `/${nameDirectory}`);
+            console.log("Веб-сайт успешно архивирован");
+        }).catch((err) => {
+            console.log("Произошла ошибка", err);
+        });
     });
-});
+}
